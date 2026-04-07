d630b17eadd75b27fda2c3f9aaf51c99ef4c5a51
h8d13 <hadean-eon-dev@proton.me>
2026-04-07 18:43:10 +0200

    move stuff

--- archinstoo/archinstoo/__init__.py
+++ archinstoo/archinstoo/__init__.py
@@ -74,7 +74,7 @@ from .lib.hardware import SysInfo
 from .lib.output import FormattedOutput, debug, error, info, log, logger, warn
 from .lib.translationhandler import Language, tr, translation_handler
 from .lib.tui.curses_menu import Tui
-from .lib.utils.env import Os, _run_script, clean_cache, ensure_keyring_initialized, ensure_pacman_configured, is_root, is_venv, kernel_info, reload_python
+from .lib.utils.env import Os, _run_script, clean_cache, ensure_keyring_initialized, is_root, is_venv, kernel_info, reload_python
 from .lib.utils.net import ping
 
 if TYPE_CHECKING:
@@ -126,6 +126,8 @@ def _bootstrap() -> int:
 		return 0
 
 	try:
+		info('Fetching db...')
+		Pacman.run('-Sy', peek_output=True)
 		debug('Fetching deps...')
 		Pacman.run(f'-S --needed --noconfirm {" ".join(hard_depends)}', peek_output=True)
 		# mark in current env as bootstraped
@@ -174,10 +176,8 @@ def _prepare() -> int:
 		# note indent fully offlines installs should be possible
 		# instead of importing full handler use sys.argv directly
 		try:
-			ensure_pacman_configured()
+			# ensure_pacman_configured() moved to pm/mirrors
 			ensure_keyring_initialized()
-			info('Fetching db...')
-			Pacman.run('-Sy', peek_output=True)
 			# non-Arch hosts have deps pre-installed via host package manager
 			if not non_arch_host and (rc := _bootstrap()):
 				return rc
