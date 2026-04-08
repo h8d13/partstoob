df172fa95a862776556fbe6a012eb48420455d03
h8d13 <hadrien.loge@gmail.com>
2026-04-09 00:31:05 +0200

    more paths

--- archinstoo/archinstoo/lib/pm/bootstrap.py
+++ archinstoo/archinstoo/lib/pm/bootstrap.py
@@ -13,8 +13,11 @@ from archinstoo.lib.output import info
 
 _PACMAN_CONF = Path('/etc/pacman.conf')
 _PACMAN_D = Path('/etc/pacman.d')
+_PACMAN_GNUPG = _PACMAN_D / 'gnupg'
+_PACMAN_HOOKS = _PACMAN_D / 'hooks'
 _PACMAN_DB = Path('/var/lib/pacman')
 _PACMAN_CACHE = Path('/var/cache/pacman/pkg')
+_PACMAN_LOG = Path('/var/log')
 _MIRRORLIST = _PACMAN_D / 'mirrorlist'
 _KEYRING_DIR = Path('/usr/share/pacman/keyrings')
 _ARCHLINUX_KEYRING = _KEYRING_DIR / 'archlinux.gpg'
@@ -124,8 +127,11 @@ def pacman_conf() -> None:
 	info('Configuring pacman for non-Arch host...')
 
 	_PACMAN_D.mkdir(parents=True, exist_ok=True)
+	_PACMAN_GNUPG.mkdir(parents=True, exist_ok=True)
+	_PACMAN_HOOKS.mkdir(parents=True, exist_ok=True)
 	_PACMAN_DB.mkdir(parents=True, exist_ok=True)
 	_PACMAN_CACHE.mkdir(parents=True, exist_ok=True)
+	_PACMAN_LOG.mkdir(parents=True, exist_ok=True)
 	info(f'Writing {_MIRRORLIST}...')
 	_MIRRORLIST.write_text(_fetch_mirrorlist())
 
